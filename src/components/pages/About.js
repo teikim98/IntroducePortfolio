f605550fe36190859eb2f30f9b65b5fbdcc953c4
teikim98 <teikim98@gmail.com>
2023-01-02 18:15:13 +0900

    about article center

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -31,6 +31,7 @@ const Introduce = styled(motion.div)`
 const Skill = styled(motion.ul)`
   display: flex;
   flex-direction: row;
+  justify-content: center;
   margin-bottom: 3.125rem;
   > li {
     margin-right: 2rem;
@@ -41,6 +42,73 @@ const Skill = styled(motion.ul)`
   }
   
 `;
+
+const SelfIntro = styled(motion.ul)`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 3.125rem;
+  > li {
+    
+  }
+  align-items: center;
+  
+`;
+
+const IntroName = styled.span`
+    
+    display: inline-block;
+    padding-bottom: 0.5rem;
+    position: relative;
+    letter-spacing: 7px;
+    text-transform: uppercase;
+    font-weight: 700;
+    color: rgb(51, 51, 51);
+    font-size: 60px;
+    font-weight: 500;
+
+    &::after {
+    content: "";
+    margin-left: 1.563rem;
+    position: absolute;
+    width: 300px;
+    height: 1px;
+    background-color: rgb(0,0,0);
+    left: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+    }
+
+    &::before {
+    content: "";
+    margin-right: 1.563rem;
+    position: absolute;
+    width: 300px;
+    height: 1px;
+    background-color: rgb(0,0,0);
+    right: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+    }
+`
+
+const IntroArticle = styled.div`
+   backdrop-filter: blur(5.5px);
+    -webkit-backdrop-filter: (5.5px);
+    background: rgba(255, 255, 255, 0.30);
+    border: 1px solid rgba(255, 255, 255, 0.34);
+    border-radius: 16px; 
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.2);
+    color: #232323;
+    backdrop-filter: blur(4px);
+    -webkit-backdrop-filter: blur(4px);
+    height: 100%;
+    width: 100%;
+    padding: 10px;
+    min-width: 768px;
+    font-size: 20px;
+`
+
 
 const list = {
   hidden: {
@@ -65,19 +133,19 @@ function About() {
   return (
     <Container>
       <Introduce variants={list} initial="hidden" animate="visible">
-        <Skill variants={item}>
+        <SelfIntro variants={item}>
           <motion.li>
-            <h2>
-              Self Introduce
-            </h2>
+            <IntroName>about me</IntroName>
           </motion.li>
           <motion.li>
-            <p>
-              이러쿵 저러쿵 저는 이런사람이고 어쩌구 저쩌구 loremals colnts
-              asdnlwa dmclakn alckawmd,s kakk w adwk;aw
-            </p>
+            <IntroArticle>
+              이러쿵 저러쿵 저는 이런사람이고 어쩌구 저쩌구 loremals colnts <br></br>
+              asdnlwa dmclakn alckawmd,s kakk w adwk;aw <br></br>
+              rkskekfk aksqktk dks krkqwkdkkqw kqnlf ql nw <br></br>
+              여기까지
+            </IntroArticle>
           </motion.li>
-        </Skill>
+        </SelfIntro>
         <Skill variants={item}>
           <motion.li>
             <FontAwesomeIcon icon={faJsSquare} size="5x" />
