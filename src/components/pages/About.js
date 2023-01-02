adafff4824cd31cecfeaaed603f30542f6493020
teikim98 <teikim98@gmail.com>
2023-01-02 16:32:42 +0900

    about.js motion처리

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,49 +1,107 @@
 import styled from "styled-components";
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence, motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBootstrap,
+  faCss3,
+  faHtml5,
+  faJsSquare,
+  faNode,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Container = styled.section`
-    
-    padding-left: 5rem;
-    padding-right: 5rem;
-    padding-top: 5rem;
+  padding-left: 5rem;
+  padding-right: 5rem;
+  padding-top: 5rem;
 
 `;
 
-const Introduce = styled.motion.div`
+const Introduce = styled(motion.div)`
+  color: ${(props) => props.theme.textColor};
+  margin-bottom: 3.125rem;
+  .text {
+    line-height: 25px;
+  }
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
 
-    margin-bottom: 2rem;
-`
+const Skill = styled(motion.ul)`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 3.125rem;
+  > li {
+    margin-right: 2rem;
+    h2 {
+      font-size: x-large;
+      font-weight: 500;
+    }
+  }
+  
+`;
 
 const list = {
-    hidden: {
-      opacity: 0
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+      duration: 0.3,
     },
-    visible: {
-      opacity: 1,
-      transition: {
-        when: "beforeChildren",
-        staggerChildren: 0.2,
-        duration : 0.5
-      }
-    }
-  };
+  },
+};
 
-  const item = {
-    hidden: { opacity: 0, x: 100 },
-    visible: { opacity: 1, x: 0 }
-  };
-  
+const item = {
+  hidden: { opacity: 0, x: 100 },
+  visible: { opacity: 1, x: 0 },
+};
 
 function About() {
   return (
     <Container>
-      <Introduce variants={list} initial ='hidden' animate='visible'>
-        
+      <Introduce variants={list} initial="hidden" animate="visible">
+        <Skill variants={item}>
+          <motion.li>
+            <h2>
+              Self Introduce
+            </h2>
+          </motion.li>
+          <motion.li>
+            <p>
+              이러쿵 저러쿵 저는 이런사람이고 어쩌구 저쩌구 loremals colnts
+              asdnlwa dmclakn alckawmd,s kakk w adwk;aw
+            </p>
+          </motion.li>
+        </Skill>
+        <Skill variants={item}>
+          <motion.li>
+            <FontAwesomeIcon icon={faJsSquare} size="5x" />
+          </motion.li>
+          <motion.li>
+            <FontAwesomeIcon icon={faHtml5} size="5x" />
+          </motion.li>
+          <motion.li>
+            <FontAwesomeIcon icon={faCss3} size="5x" />
+          </motion.li>
+        </Skill>
+        <Skill variants={item}>
+          <motion.li>
+            <FontAwesomeIcon icon={faNode} size="5x" />
+          </motion.li>
+          <motion.li>
+            <FontAwesomeIcon icon={faReact} size="5x" />
+          </motion.li>
+          <motion.li>
+            <FontAwesomeIcon icon={faBootstrap} size="5x" />
+          </motion.li>
+        </Skill>
       </Introduce>
-       <motion.ul variants={list} initial="hidden" animate="visible">
-        <motion.li variants={item}>저는 이런 사람입니다 ... +++ 이런 가치관과 세계관을 가지고 있습니다 ... ++++ </motion.li>
-      </motion.ul>
-     </Container>
+    </Container>
   );
 }
 
--- src/components/pages/proCom/Article.js
+++ src/components/pages/proCom/Article.js
@@ -1,7 +1,10 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { data } from "./data";
-import { FaBeer } from 'react-icons/fa';
+
+const exitModal = () =>{
+  
+}
 
 
 const Article = ({ id }) => {
@@ -14,9 +17,8 @@ const Article = ({ id }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0, transition: { duration: 0.15 } }}
     >
-      
       <motion.div layoutId={`item-motion-${id}`}>
-        <Link to="/Project"><FaBeer/>Go Back</Link>
+        <Link to="/Project">Go Back</Link>
         <motion.div className="image-motion" layoutId={`image-motion-${id}`}>
           <img className="image" src={`./images/${id}.jpeg`} alt='' />
         </motion.div>
@@ -30,7 +32,6 @@ const Article = ({ id }) => {
           </p>
         </div>
       </motion.div>{" "}
-     
     </motion.div>
 
   );
